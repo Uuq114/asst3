8ee6b9882cffe75663eb5420c4c405bac25983f2
Uuq114 <811670442@qq.com>
2025-02-19 19:52:46 +0800

    sync

--- scan/scan.cu
+++ scan/scan.cu
@@ -48,8 +48,8 @@ __global__ void
 upsweep_phase_kernel(int* array, int N, int two_d) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int two_dplus1 = 2 * two_d;
-    if (idx % two_dplus1 == 0 && i + two_dplus1 - 1 < N) {
-        array[i + two_dplus1 - 1] += array[i + two_d - 1];
+    if (idx % two_dplus1 == 0 && idx + two_dplus1 - 1 < N) {
+        array[idx + two_dplus1 - 1] += array[idx + two_d - 1];
     }
 }
 
@@ -57,10 +57,10 @@ __global__ void
 downsweep_phase_kernel(int* array, int N, int two_d) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int two_dplus1 = 2 * two_d;
-    if (idx % two_dplus1 == 0 && i + two_dplus1 - 1 < N) {
-        int t = array[i + two_d - 1];
-        array[i + two_d - 1] = array[i + two_dplus1 - 1];
-        array[i + two_dplus1 - 1] += t;
+    if (idx % two_dplus1 == 0 && idx + two_dplus1 - 1 < N) {
+        int t = array[idx + two_d - 1];
+        array[idx + two_d - 1] = array[idx + two_dplus1 - 1];
+        array[idx + two_dplus1 - 1] += t;
     }
 }
 
