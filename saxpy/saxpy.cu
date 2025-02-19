b0806d6294ffe6670630221227c631a6d8002d42
Uuq114 <811670442@qq.com>
2025-02-19 15:37:23 +0800

    sync

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -80,7 +80,7 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     cudaMalloc(&device_result, N * sizeof(float));
 
     // start timing after allocation of device memory
-    double startTime = CycleTimer::currentSeconds();
+    double time1 = CycleTimer::currentSeconds();
 
     //
     // CS149 TODO: copy input arrays to the GPU using cudaMemcpy
@@ -90,17 +90,19 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
    
     // run CUDA kernel. (notice the <<< >>> brackets indicating a CUDA
     // kernel launch) Execution on the GPU occurs here.
+    double time2 = CycleTimer::currentSeconds();
     saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
     cudaDeviceSynchronize();
 
     //
     // CS149 TODO: copy result from GPU back to CPU using cudaMemcpy
     //
+    double time3 = CycleTimer::currentSeconds();
     cudaMemcpy(resultarray, device_result, N, cudaMemcpyDeviceToHost);
 
     
     // end timing after result has been copied back into host memory
-    double endTime = CycleTimer::currentSeconds();
+    double time4 = CycleTimer::currentSeconds();
 
     cudaError_t errCode = cudaPeekAtLastError();
     if (errCode != cudaSuccess) {
@@ -108,8 +110,11 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
 		errCode, cudaGetErrorString(errCode));
     }
 
-    double overallDuration = endTime - startTime;
+    double overallDuration = time4 - time1;
+    double kernelDuration = time3 - time2;
     printf("Effective BW by CUDA saxpy: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, GBPerSec(totalBytes, overallDuration));
+    printf("[copy data to GPU + run kernel + copy data back to CPU]: %.3f ms", 1000.f * overallDuration);
+    printf("[run kernel]: %.3f ms", 1000.f * kernelDuration);
 
     //
     // CS149 TODO: free memory buffers on the GPU using cudaFree
